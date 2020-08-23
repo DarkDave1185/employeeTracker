e6bb24f220dd16d6dfd5538f5c371be6a3a11201
DarkDave <darkdave1185@gmail.com>
2020-08-23 12:12:32 -0700

    late homework behind attempt commit each section of homework/ created basic schema

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE department (
+    ID INT Primary KEY,
+    Dept_Name VARCHAR(30)
+);
+
+CREATE TABLE employeeRole (
+    ID INT PRIMARY KEY,
+    Title VARCHAR(30),
+    Salary DECIMAL(10,2),
+    DepartmentID INT
+);
+
+CREATE TABLE employee (
+    ID INT PRIMARY KEY,
+    FirstName VARCHAR(30),
+    LastName VARCHAR(30),
+    RoleID INT,
+    ManagerID INT NULL
+);
